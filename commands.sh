d9f2ae50ad123eaca53fae635a01302dcc881fa0
apkap83 <33512036+apkap83@users.noreply.github.com>
2022-11-05 09:27:05 +0200

    Until git stash

--- commands.sh
+++ commands.sh
@@ -43,10 +43,50 @@ git restore --staged path/to/unwanted_file
     # if we do not have conflicting changes (new file), we will be able to switch branch
 
     # Delete a branch (have to be away from the branch)
-    git branch -d <branch-name>   OR Force due to not merged yet   git branc -D <branch-name>
+    git branch -d <branch-name> -OR Force due to not merged yet- git branch -D <branch-name>
 
     # Move/Rename branch (have to be in the branch)
     git branch -m <new-branch-name>
 
+### DIFF ###
 
-    
+    # Compares Working Directory and Staging Area
+    git diff [filenames]
+
+    # Compares Stage and HEAD
+    # "Show me what will be included in my commit if I run git commit right now"
+    git diff --staged [filenames]
+
+    # Compares Working Directory and HEAD
+    git diff HEAD [filenames]
+
+    # Compares 2 different commits
+    git diff commit1..commit2 -OR- commit1 commit2
+
+    # Compare 2 different Branches
+    git diff branch1..branch2 -OR- git diff branch1 branch2
+
+### GIT STASH ###
+
+    # Running git stash will take all uncommitted changes (staged and unstaged)
+    # and stash them, reverting the changes in your working copy.
+    git stash -OR- git stash save
+
+    # View all stashses
+    git stash list
+
+    # Remove the most recently stashed changes in your stash and re-apply them to your
+    # working Directory 
+    git stash pop
+
+    # Apply whatever is stashed away, without removing it from the stash. This can be useful
+    # if you want to apply stashed changes to multiple branches
+    git stash apply
+    -OR- git stash apply stash@{0}  # When you have multiple stashes
+
+    # Delete a particular stash
+    git stash drop stash@{0}
+
+    # Clear all stash
+    git stash clear
+
